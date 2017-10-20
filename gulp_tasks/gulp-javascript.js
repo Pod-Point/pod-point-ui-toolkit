d9faba52001ce533f265029f4b9f92af4e764777
threesquared <ben@3sq.re>
2017-10-20 15:45:13 +0100

    Export js modules

--- gulp_tasks/gulp-javascript.js
+++ gulp_tasks/gulp-javascript.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
 const config = require('../config');
 const webpackConfig = require('../webpack.config.js');
 const webpack = require('webpack-stream');
@@ -7,7 +8,9 @@ const browserSync = require('browser-sync').create();
 
 gulp.task('jsModules', () => {
     return gulp.src(config.src.js + '**/*.js')
+        .pipe(sourcemaps.init())
         .pipe(babel())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.dist.js))
         .pipe(browserSync.stream());
 });
---
+++ src/assets/js/index.js
@@ -0,0 +1,11 @@
+import accordion from './modules/accordion';
+import base from './modules/base';
+import toggleElement from './modules/toggle-element';
+import typeahead from './modules/typeahead';
+
+export {
+    accordion,
+    base,
+    toggleElement,
+    typeahead,
+};
