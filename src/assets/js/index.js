f9e0e3a4bb501150620a6c02348ff576981e4bfc
threesquared <ben@3sq.re>
2018-05-08 12:49:32 +0100

    Remove cookie notice JS

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,11 +2,9 @@ import './modules/base';
 import accordion from './modules/accordion';
 import toggleElement from './modules/toggle-element';
 import typeahead from './modules/typeahead';
-import cookieNotice from './modules/cookie-notice';
 
 export {
     accordion,
     toggleElement,
     typeahead,
-    cookieNotice,
 };
